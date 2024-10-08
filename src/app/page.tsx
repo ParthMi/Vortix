08d2b3111c3d9cdd556edc6727e568b1d6149c02
ParthM <parthmiroliya72784@gmail.com>
2024-10-08 23:05:58 +0530

    - Change title to Vortix Enterprise.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import Video from "@/components/Video";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Free Next.js Template for Startup and SaaS",
+  title: "Vortix Enterprise",
   description: "This is Home for Startup Nextjs Template",
   // other metadata
 };
