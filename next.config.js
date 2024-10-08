a093e79ba3bbd083808a30b66c55ac8360a1c515
ParthM <parthmiroliya72784@gmail.com>
2024-10-08 22:57:17 +0530

    - Initial commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    // You can set this to any domain if you want to allow from specific domains, or use * as hostname to allow everything
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // Allow all domains using HTTPS
+      },
+      {
+        protocol: "http",
+        hostname: "**", // Allow all domains using HTTP
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Link from "next/link";
+
+const Hero = () => {
+  return (
+    <>
+      <style jsx>{`
+     #home {
+      position: relative;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      overflow: hidden;
+    }
+  
+    .background-video {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      z-index: -1; /* Make sure video stays behind content */
+    }
+
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(255, 255, 255, 0.5); /* White background with 50% opacity */
+  z-index: 0; /* Ensure overlay is above video but below content */
+}
+      `}</style>
+
+      <section
+        id="home"
+        className="relative z-10 overflow-hidden pb-16 pt-[120px] dark:bg-gray-dark md:pb-[120px] md:pt-[150px] xl:pb-[160px] xl:pt-[180px] 2xl:pb-[200px] 2xl:pt-[210px]"
+      >
+        <video autoPlay loop muted playsInline className="absolute top-0 left-0 w-full h-full object-cover z-0">
+          <source src="/bg.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <div className="container relative z-10">
+          <div className="-mx-4 flex flex-wrap">
+            <div className="w-full px-4">
+              <div className="mx-auto max-w-[800px] text-center">
+                <h1 className="mb-5 text-3xl font-bold leading-tight text-black dark:text-white sm:text-4xl sm:leading-tight md:text-5xl md:leading-tight">
+                  Global Leaders in High-Quality Activated Carbon Solution
+                </h1>
+                <p className="mb-12 text-base !leading-relaxed text-body-color dark:text-body-color-dark sm:text-lg md:text-xl">
+                  Providing superior Activated Carbon for wastwater treatment, air purification, oil refineries and more. Our commitment to quality and reliability ensures the best solution for your industry, backed by exceptional customer service.
+                </p>
+                <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+                  <Link
+                    href=""
+                    className="inline-block rounded-sm bg-black px-8 py-4 text-base font-semibold text-white duration-300 ease-in-out hover:bg-black/90 dark:bg-white/10 dark:text-white dark:hover:bg-white/5"
+                  >
+                    Read More
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
